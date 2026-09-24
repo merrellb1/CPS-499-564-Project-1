59762ebb6df43f2fbf02db430ba9c0f7c7519874
Ben Merrell <merrelb1@udayton.edu>
2026-09-23 20:47:13 -0400

    added RGB and RGBD data

---
+++ RGB Data/RGB Data/test.m
@@ -0,0 +1,12 @@
+clear all;
+close all;
+clc;
+
+img1r = imread("1_HW1_1.jpg");
+figure, imshow(img1r);
+img1g = imread("1_HW1_2.jpg");
+figure, imshow(img1g);
+img1b = imread("1_HW1_3.jpg");
+figure, imshow(img1b);
+
+
